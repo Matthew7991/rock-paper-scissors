615c59891feca20972d7bb7762405facd72e1880
Matthew7991 <Matthew7991@gmx.de>
2023-07-14 15:07:52 +0200

    changed class names, getting value from label

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,49 +1,44 @@
 "use strict";
 
 // rounds
-const roundsInput = document.querySelector(".rounds-input")
-const roundsOutput = document.querySelector(".rounds-output")
+const roundsInput = document.querySelector(".rpc-rounds-input")
+const roundsOutput = document.querySelector(".rpc-rounds-output")
 
 function createInputRadios(amount, steps) {
   for (let i = 0; i < amount; i++) {
     const inputRadioTemplate = 
-    `<div>
-      <input
-        type="radio"
-        name="roundsCount"
-        id="round${i*steps+5}"
-        value="${i*steps+5}"
-      />
-      <label for="round${i*steps+5}">${i*steps+5}</label>
-    </div>`
+      `<div>
+        <input
+          type="radio"
+          name="roundsCount"
+          id="round${i*steps+5}"
+        />
+        <label for="round${i*steps+5}">${i*steps+5}</label>
+      </div>`
     roundsInput.insertAdjacentHTML("beforeend", inputRadioTemplate)
   }
   document.querySelector("[name='roundsCount'").checked = true
 }
-
 createInputRadios(4,5)
 
-console.log( document.querySelector("[name='roundsCount'").setAttribute("checked", ""))
-
 // Round Counter
-const roundCounter = document.querySelector(".rounds-counter")
+const roundCounter = document.querySelector(".rpc-rounds-counter")
 let roundCurrentValue = 0
 let roundMaxValue = 0
 
 // Score
-const score = document.querySelector(".score")
+const score = document.querySelector(".rpc-score")
 let scoreUserValue = 0
 let scoreComputerValue = 0
 
 // Text Output
-const textOutput = document.querySelector(".output")
+const textOutput = document.querySelector(".rpc-output")
 
 //  Buttons
-const rpsBtns = document.querySelectorAll(".buttons button")
+const choiceButtons = document.querySelectorAll(".rpc-choice-buttons button")
 
 const choices = ["Rock", "par Bier", "Sister"]
-const choiceAmounts = rpsBtns.length
-
+const choiceAmounts = choiceButtons.length
 
 function toggleRoundsDisplay() {
   roundsInput.classList.toggle("hidden")
@@ -58,6 +53,12 @@ function setScoreDisplay() {
   score.textContent = `${scoreUserValue} : ${scoreComputerValue}`
 }
 
+function removeChoiceButtonsBgColor() {choiceButtons.forEach(button => {
+  button.classList.remove("rpc-win")
+  button.classList.remove("rpc-loose")
+  button.classList.remove("rpc-draw")
+})}
+
 function rps(event) {
   if (roundCurrentValue == roundMaxValue && roundMaxValue != 0) {
     return
@@ -65,25 +66,32 @@ function rps(event) {
   
   if (roundMaxValue == 0) {
     toggleRoundsDisplay()
-    roundMaxValue = Number(document.querySelectorAll('input[type="radio"]:checked')[0].value)
+    roundMaxValue = Number(document.querySelectorAll('input[type="radio"]:checked')[0].nextElementSibling.textContent)
   }
 
   roundCurrentValue++
   setRoundDisplay()
 
+  removeChoiceButtonsBgColor()
+
+  const userChoiceButton = event.target.closest("button")
   const computerChoice = Math.floor(Math.random() * choiceAmounts)
-  const userChoice = Number(event.target.closest("button").value)
+  const userChoice = Number(userChoiceButton.value)
 
   const winner = (userChoice - computerChoice + choiceAmounts) % choiceAmounts
 
   if (winner == 0) {
     textOutput.textContent = `It's a draw. You both chose ${choices[userChoice]}`
+    userChoiceButton.classList.add("rpc-draw")
   } else if (winner == 1) {
   textOutput.textContent = `${choices[userChoice]} beats ${choices[computerChoice]}. You win!`
   scoreUserValue++
+  userChoiceButton.classList.add("rpc-win")
+
   } else if (winner == 2) {
   textOutput.textContent = `${choices[computerChoice]} beats ${choices[userChoice]}. You loose!`
   scoreComputerValue++
+  userChoiceButton.classList.add("rpc-loose")
   }
 
   setScoreDisplay()
@@ -103,7 +111,9 @@ function rpsReset() {
   if (roundMaxValue == 0) {
     return
   }
+
   toggleRoundsDisplay()
+  removeChoiceButtonsBgColor()
 
   roundCurrentValue = 0
   scoreUserValue = 0
@@ -116,8 +126,5 @@ function rpsReset() {
 }
 
 // Event Listeners
-document.querySelector(".reset").addEventListener("click", rpsReset)
-rpsBtns.forEach(btn => btn.addEventListener("click", rps))
-
-// get value from label instead of input value
-// assemble input and label with javascript from template with steps and count aka 4 buttons in steps of 5
+document.querySelector(".rpc-reset").addEventListener("click", rpsReset)
+choiceButtons.forEach(btn => btn.addEventListener("click", rps))
